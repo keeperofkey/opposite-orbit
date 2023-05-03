cba24849c4a36815443886d2b170d4500aa08dcb
keeperofkey <doddtastic@gmail.com>
2023-05-03 10:58:38 -0400

    assets-images

---
+++ src/content/config.ts
@@ -0,0 +1,15 @@
+import { defineCollection, z } from "astro:content";
+
+const blogCollection = defineCollection({
+	schema: ({ image }) => z.object({
+		title: z.string(),
+		cover: image().refine((img) => img.width >= 1080, {
+			message: "image must be at least 1080p"
+		}),
+		coverAlt: z.string(),
+	});
+});
+
+export const collections = {
+	blog: blogCollection,
+};
